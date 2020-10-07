0f120f83cc807e840ce3ed59ca966051d5089d69
TarabrinJa <tarabrinjacob@gmail.com>
2020-10-07 11:30:58 +0300

    Done

--- src/logic.js
+++ src/logic.js
@@ -1,15 +1,18 @@
 let licenseToggle = document.querySelectorAll('input[name=licenseToggle]');
-let checkedPlan = document.querySelector('input[name=licenseToggle][checked]');
-
-console.log(licenseToggle);
-console.log(checkedPlan);
+let outputPlan = document.querySelector('.output-plan');
+let outputCost = document.querySelector('.output-cost');
+let licenseNumber = document.querySelector('.license-number');
 
 for (let plan of licenseToggle) {
 	plan.onclick = function() {
-		let licensePlan = checkedPlan.value;
-		let licensePrice = checkedPlan.dataset.licensePrice;
-		console.log(licensePlan);
-		console.log(licensePrice);
+		let licensePlan = plan.value;
+		let licensePrice = plan.dataset.licensePrice;
+		let licenseNumberValue = licenseNumber.value;
+		console.log('licensePlan: ' + licensePlan);
+		console.log('licensePrice: ' + licensePrice);
+		console.log('licenseNumber: ' + licenseNumber.value);
+		outputPlan.textContent = licensePlan;
+		outputCost.textContent = licensePrice * licenseNumberValue;
 	};
 }
 
